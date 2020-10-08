6cbc291472875cc3baac11c8fd730bbf5c78be8b
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-10-08 16:49:51 -0300

    Get location and weather

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,13 +1,54 @@
+const fetch = require("node-fetch");
+
+const getIP = (req) => {
+  return (
+    req.headers["x-forwarded-for"] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    (req.connection.socket ? req.connection.socket.remoteAddress : null)
+  ).split(",")[0];
+};
+
+const getCity = async (req) => {
+  var ip = getIP(req);
+  const ipRes = await fetch(`http://ip-api.com/json/${ip}`);
+  const { city } = await ipRes.json();
+  return city;
+};
+
+const openWeatherApiKey = process.env.OPEN_WEATHER_API_KEY;
+
 module.exports = {
-  location: (req, res, next) => {
-    res.status(200).json({ status: "ok" });
+  location: async (req, res, next) => {
+    try {
+      const city = await getCity(req);
+      res.status(200).json(city);
+    } catch (err) {
+      res.status(400).json({ err });
+    }
   },
-  current: (req, res, next) => {
-    const { city } = req.params;
-    res.status(200).json({ status: "ok", city });
+  current: async (req, res, next) => {
+    try {
+      const city = req.params.city || (await getCity(req));
+      const currentRes = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeatherApiKey}`
+      );
+      const current = await currentRes.json();
+      res.status(200).json({ status: "ok", city, current });
+    } catch (err) {
+      res.status(400).json({ err });
+    }
   },
-  forecast: (req, res, next) => {
-    const { city } = req.params;
-    res.status(200).json({ status: "ok", city });
+  forecast: async (req, res, next) => {
+    try {
+      const city = req.params.city || (await getCity(req));
+      const forecastRes = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${openWeatherApiKey}`
+      );
+      const forecast = await forecastRes.json();
+      res.status(200).json({ status: "ok", city, forecast });
+    } catch (err) {
+      res.status(400).json({ err });
+    }
   },
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const router = require("express-promise-router")();
 const Controller = require("../controllers");
 
 router.route("/location").get(Controller.location);
-router.route("/current/:city").get(Controller.current);
-router.route("/forecast/:city").get(Controller.forecast);
+router.route("/current/:city?").get(Controller.current);
+router.route("/forecast/:city?").get(Controller.forecast);
 
 module.exports = router;
