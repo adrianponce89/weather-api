0cab220141f480a7a426a1ffb07a0057d84325e6
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-10-09 09:03:29 -0300

    Add cords

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 // app.js
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+// Cors
+app.use(cors());
 
 // Routes
 app.use("/v1", require("./routes"));
