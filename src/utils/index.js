3a7ac83515927adc523848b17550acbb6123a94c
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-10-14 11:35:03 -0300

    Use weather at noon for forecast

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,34 @@
+// const singleForecastItem = {
+//   dt: 1602255600,
+//   main: {
+//     temp: 299.53,
+//     feels_like: 298.01,
+//     temp_min: 299.53,
+//     temp_max: 300.11,
+//     pressure: 1022,
+//     sea_level: 1022,
+//     grnd_level: 948,
+//     humidity: 33,
+//     temp_kf: -0.58,
+//   },
+//   weather: [
+//     { id: 803, main: 'Clouds', description: 'broken clouds', icon: '04d' },
+//   ],
+//   clouds: {
+//     all: 73,
+//   },
+//   wind: {
+//     speed: 1.79,
+//     deg: 303,
+//   },
+//   visibility: 10000,
+//   pop: 0,
+//   sys: {
+//     pod: 'd',
+//   },
+//   dt_txt: '2020-10-09 15:00:00',
+// };
+
 module.exports = {
   convert3HourForecastToDaily: (list) => {
     const daily = [];
@@ -7,6 +38,8 @@ module.exports = {
       let accFeels_like = 0;
       let temp_min = slicedList[0].main.temp_min;
       let temp_max = slicedList[0].main.temp_max;
+      let weather = slicedList[0].weather;
+      let dt_txt = slicedList[0].dt_txt;
 
       slicedList.forEach((item) => {
         accTemp += item.main.temp;
@@ -15,6 +48,11 @@ module.exports = {
           item.main.temp_min < temp_min ? item.main.temp_min : temp_min;
         temp_max =
           item.main.temp_max > temp_max ? item.main.temp_max : temp_max;
+
+        if (item.dt_txt.substring(11) === '12:00:00') {
+          weather = item.weather;
+          dt_txt = item.dt_txt;
+        }
       });
 
       daily[index] = {
@@ -24,8 +62,8 @@ module.exports = {
           temp_min,
           temp_max,
         },
-        weather: slicedList[4].weather,
-        dt_txt: slicedList[4].dt_txt.substring(0, 10),
+        weather,
+        dt_txt,
       };
     }
 
